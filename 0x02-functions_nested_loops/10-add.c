d6dfb099f49b832d332f987fa26eae48e97c7714
ziidahnabs <nabukenyaziidah@yahoo.com>
2023-02-22 14:23:21 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - Adds the two integers
+*@n1: The first integer to be added
+*@n2: The second integer to be added
+*Return: Results
+*/
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,9 @@
+#include "main"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	
