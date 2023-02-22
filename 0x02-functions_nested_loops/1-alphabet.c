5451e00eb362cd1ca4247cbec4fb9ec8cf375023
ziidahnabs <nabukenyaziidah@yahoo.com>
2023-02-22 01:56:12 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - print lowercase alphabets
+ * Return: void
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+return (void);
+}
