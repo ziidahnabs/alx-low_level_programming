aa23e9a3b4c6f70141a6e124b3c6f2ff288ce922
ziidahnabs <nabukenyaziidah@yahoo.com>
2023-02-23 09:39:26 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers except 2 and 4
+ * Return: void
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		_putchar(n);
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 t0 14
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	
