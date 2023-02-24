540f89989de4d3bad1ba4f90209d1a3aff46fed2
ziidahnabs <nabukenyaziidah@yahoo.com>
2023-02-23 23:48:31 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,14 @@
  * main - print the alphabet in lowercase
  * Return: 0
  */
+
 int main(void)
 {
 	char c;
 
-	for (c >= 'a' && c <= 'z')
-		_putchar(c);
-	_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
 }
