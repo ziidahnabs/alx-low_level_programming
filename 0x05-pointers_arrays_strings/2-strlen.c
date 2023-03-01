8eb5cb21c735aa7f0a0f61b571ed282a017d71f7
ziidahnabs <nabukenyaziidah@yahoo.com>
2023-03-01 01:46:21 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * _strlen - return the length of a string
- * Owned By Bwave
  * @s: char to check
  * Description: this will return the length of a string
  * Return: 0 is success
